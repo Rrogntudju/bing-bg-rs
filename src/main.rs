6a0d1e9b6debe8dcf8a2d180c2d1dfe9921e87b1
Rrogntudju <mario.prince@outlook.com>
2019-01-25 19:37:58 -0500

    jpg

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,46 @@ use serde_json::value::Value;
 use reqwest::Client;
 use std::error::Error;
 use std::fmt;
+use image::{ImageFormat, load_from_memory_with_format};
+
+const URL_DESC: &str = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
 
 #[derive(Debug)]
 pub enum BingError {
-    InvalidImageUrl
+    InvalidImageUrl,
 }
 
 impl fmt::Display for BingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             BingError::InvalidImageUrl => write!(f, 
-                "La propriété «url» n'est pas dans le descriptif JSON. 
-                Vérifier dans https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"),
+                "La propriété «url» n'est pas dans le descriptif JSON. \nVérifier dans {}",
+                URL_DESC),
         }
     }
 }
 
 impl Error for BingError {}
 
-fn main() -> Result<(), Box<Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Téléchargement du descriptif de l'image...");
     let client = Client::builder().build()?;
-    let desc: Value = client.get("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").send()?.json()?;
+    let desc: Value = client.get(URL_DESC).send()?.json()?;
     let url = desc["images"][0]["url"].as_str();
-    if url.is_some() {
-        let url_img = "https://www.bing.com".to_owned() + url.unwrap();
+    if url.is_none() {
+        return Err(From::from(BingError::InvalidImageUrl));
     }
-    else {
-        return Err(Box::new(BingError::InvalidImageUrl));
-    }
-    
+    let url_img = "https://www.bing.com".to_owned() + url.unwrap();
 
+    println!("Téléchargement de l'image JPEG...");
+    let mut resp = client.get(&url_img).send()?;
+
+    println!("Transformer en BMP et sauvegarder...");
+    let mut buf: Vec<u8> = vec![];
+    resp.copy_to(&mut buf)?;
+    let img = load_from_memory_with_format(&buf, ImageFormat::JPEG)?;
+    
+ 
 
     Ok(())
 }
