1b3131288bc92c0d80e95fdab5acb23b0e576efe
Rrogntudju <mario.prince@outlook.com>
2019-10-12 10:48:56 -0400

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // Téléchargement de l'image Bing du jour et installation comme arrière-plan dans Windows
 use {
-    serde_json::value::Value,
-    reqwest::Client,
-    std::error::Error,
-    std::{fmt, env},
-    image::{ImageFormat, load_from_memory_with_format},
     core::ffi::c_void,
-    winapi::um::winuser::{SystemParametersInfoW, SPI_SETDESKWALLPAPER, SPIF_UPDATEINIFILE, SPIF_SENDCHANGE},
+    image::{load_from_memory_with_format, ImageFormat},
+    reqwest::Client,
+    serde_json::value::Value,
+    std::error::Error,
+    std::{env, fmt},
+    winapi::um::winuser::{
+        SystemParametersInfoW, SPIF_SENDCHANGE, SPIF_UPDATEINIFILE, SPI_SETDESKWALLPAPER,
+    },
 };
 
 const URL_DESC: &str = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
@@ -16,7 +18,7 @@ struct BingError(String);
 
 impl fmt::Display for BingError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-         write!(f, "{}", self.0)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -28,7 +30,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let desc: Value = client.get(URL_DESC).send()?.json()?;
     let url = desc["images"][0]["url"].as_str();
     if url.is_none() {
-        return Err(BingError(format!("La propriété «url» est absente du descriptif JSON. Vérifier dans {}", URL_DESC)).into());
+        return Err(BingError(format!(
+            "La propriété «url» est absente du descriptif JSON. Vérifier dans {}",
+            URL_DESC
+        ))
+        .into());
     }
     let url_img = "https://www.bing.com".to_owned() + url.unwrap();
 
@@ -43,22 +49,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     bmp_path.push("bingbg");
     bmp_path.set_extension("bmp");
     img.save(&bmp_path)?;
- 
+
     println!("Configurer l'image comme arrière-plan...");
     //convertir la chaîne utf-8 en chaîne utf-16 terminée par null
     let mut path: Vec<u16> = bmp_path.to_str().unwrap().encode_utf16().collect();
-    path.push(0); 
+    path.push(0);
     let path_ptr = path.as_mut_ptr() as *mut c_void;
-    
-    let rc = unsafe { SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_ptr , SPIF_UPDATEINIFILE | SPIF_SENDCHANGE) };
+
+    let rc = unsafe {
+        SystemParametersInfoW(
+            SPI_SETDESKWALLPAPER,
+            0,
+            path_ptr,
+            SPIF_UPDATEINIFILE | SPIF_SENDCHANGE,
+        )
+    };
     if rc == 0 {
-        return 
-            match std::io::Error::last_os_error().raw_os_error() {
-                Some(e) => Err(BingError(format!("SystemParametersInfoW a retourné le code d'erreur {}", e)).into()),
-                None    => Err(BingError("Oups!".into()).into()),
-            }
+        return match std::io::Error::last_os_error().raw_os_error() {
+            Some(e) => Err(BingError(format!(
+                "SystemParametersInfoW a retourné le code d'erreur {}",
+                e
+            ))
+            .into()),
+            None => Err(BingError("Oups!".into()).into()),
+        };
     }
-   
+
     println!("Terminé!");
     Ok(())
 }
