74a3c5353885c168bcb13be8af7e6a580264147d
Rrogntudju <rrogntudju@example.com>
2021-06-13 14:35:13 -0400

    windows 0.11

--- build/build.rs
+++ build/build.rs
@@ -1,10 +1,10 @@
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("build/icon.ico").compile().unwrap();
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("build/icon.ico").compile().unwrap();
 
-        windows::build!(
-            Windows::Win32::UI::WindowsAndMessaging::{SystemParametersInfoW, SPI_SETDESKWALLPAPER, SPIF_UPDATEINIFILE, SPIF_SENDCHANGE},
-        );
-    }
+    windows::build!(
+        Windows::Win32::UI::WindowsAndMessaging::{
+            SystemParametersInfoW, SYSTEM_PARAMETERS_INFO_ACTION, SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS, SYSTEM_PARAMETERS_INFO_UPDATE_FLAGS
+        },
+    );
 }
