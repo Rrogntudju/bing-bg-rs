bf1dc954d2f14bd76009cebc4b3be04303b10039
Rrogntudju <rrogntudju@example.com>
2025-05-10 13:05:20 -0400

    wip

--- build/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("build/icon.ico").compile().unwrap();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-// Téléchargement de l'image Bing du jour et installation comme arrière-plan dans Windows
-use windows::Win32::UI::WindowsAndMessaging::{SystemParametersInfoW, SPIF_SENDCHANGE, SPIF_UPDATEINIFILE, SPI_SETDESKWALLPAPER};
+// Téléchargement de l'image Bing du jour et installation comme arrière-plan dans Cosmic
 use {
-    core::ffi::c_void,
     image::{load_from_memory_with_format, ImageFormat},
     reqwest::Client,
     serde_json::value::Value,
@@ -36,18 +34,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     img.save(&bmp_path)?;
 
     println!("Configurer l'image comme arrière-plan...");
-    //convertir la chaîne utf-8 en chaîne utf-16 terminée par null
-    let mut path: Vec<u16> = bmp_path.to_str().unwrap().encode_utf16().collect();
-    path.push(0);
-    let path_ptr = Some(path.as_mut_ptr() as *mut c_void);
 
-    let rc = unsafe { SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path_ptr, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE) };
-    if rc.is_err() {
-        return Err(match std::io::Error::last_os_error().raw_os_error() {
-            Some(e) => format!("SystemParametersInfoW a retourné le code d'erreur {}", e).into(),
-            None => "Oups!".into(),
-        });
-    }
 
     println!("Terminé!");
     Ok(())
