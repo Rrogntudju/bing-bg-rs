e7e5ca17e68902c9cddb5138a872440424aace46
Rrogntudju <rrogntudju@example.com>
2025-05-14 04:28:24 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let task = tokio::spawn(async move { client.get(&url_img).send().await?.bytes().await });
 
     println!("Téléchargement de l'image JPEG...");
-    if let Some(home) = env::vars().find(|v| v.0 == "HOME").map(|v| v.1) {
-        let bg_path = Path::new(&home).join(".Bingbg");
-        if !bg_path.exists() {
-            create_dir(&bg_path)?;
-        }
-        let img = task.await??.to_vec();
-        let img = load_from_memory_with_format(&img, ImageFormat::Jpeg)?;
-        let bg_path = bg_path.with_file_name("bingbg.jpg");
-        img.save(&bg_path)?;
-
-        println!("Configurer l'image comme arrière-plan...");
+    let home = if let Some(home) = env::vars().find(|v| v.0 == "HOME").map(|v| v.1) {
+        home
     } else {
         return Err(format!("La variable d'environment HOME n'est pas configurée").into());
+    };
+    let bg_path = Path::new(&home).join(".Bingbg");
+    if !bg_path.exists() {
+        create_dir(&bg_path)?;
     }
+    let img = task.await??.to_vec();
+    let img = load_from_memory_with_format(&img, ImageFormat::Jpeg)?;
+    let bg_path = bg_path.with_file_name("bingbg.jpg");
+    img.save(&bg_path)?;
+
+    println!("Configurer l'image comme arrière-plan...");
 
     println!("Terminé!");
     Ok(())
