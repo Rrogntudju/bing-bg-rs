d71c3830c02e29171ad2ffacce1548cb49053ee1
Rrogntudju <rrogntudju@example.com>
2025-07-08 21:58:42 -0400

    titre

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use {
     cosmic_bg_config::{self, Source},
     reqwest::Client,
     serde_json::value::Value,
-    std::{env, error::Error, fs, io::Write, path::Path, time::Duration},
+    std::{env, error::Error, fs, io::Write, path::{Path, PathBuf}, time::Duration},
 };
 
 const URL_DESC: &str = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
@@ -14,12 +14,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
     let response = client.get(URL_DESC).send().await?.text().await?;
     let desc: Value = serde_json::from_str(&response)?;
-    let url = if let Some(url) = desc["images"][0]["url"].as_str() {
-        url
+    let url_img = if let Some(url) = desc["images"][0]["url"].as_str() {
+        "https://www.bing.com".to_owned() + url
     } else {
-        return Err(format!("La propriété «url» est absente du descriptif JSON. Vérifier dans {}", URL_DESC).into());
+        return Err(format!("La propriété «url» est absente du descriptif JSON. Vérifier dans {URL_DESC}").into());
     };
-    let url_img = "https://www.bing.com".to_owned() + url;
 
     // Téléchargement de l'image JPEG
     let download_task = tokio::spawn(async move { client.get(&url_img).send().await?.bytes().await });
@@ -32,10 +31,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if !bg_path.exists() {
         fs::create_dir(&bg_path)?;
     }
-    let bg_file = bg_path.join("bingbg.jpg");
-    
-    let img = download_task.await??.to_vec();
+    let bg_file = if let Some(titre) = desc["images"][0]["title"].as_str() {
+        bg_path.join(titre.to_owned() + ".jpg")
+    } else {
+        return Err(format!("La propriété «title» est absente du descriptif JSON. Vérifier dans {URL_DESC}").into());
+    };
     let mut bg = fs::File::create(&bg_file)?;
+    let img = download_task.await??.to_vec();
     bg.write_all(&img)?;
 
     // Configurer l'image comme arrière-plan
@@ -44,6 +46,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     background.source = Source::Path(bg_file);
     let mut config = cosmic_bg_config::Config::load(&context)?;
     config.set_entry(&context, background)?;
+
+    //Ménage
+    let mut bg_entries: Vec<(Duration, PathBuf)> = Vec::new();
+    for entry in fs::read_dir(bg_path)? {
+        let entry = entry?;
+        bg_entries.push((entry.metadata()?.created()?.elapsed()?, entry.path()));
+    }
+    bg_entries.sort_by_key(|(d, _)| *d);
+    bg_entries.iter().skip(7).for_each(|(_, p)| fs::remove_file(p).unwrap_or_default());
 
     Ok(())
 }
